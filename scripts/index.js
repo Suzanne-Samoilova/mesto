12b852b3279b7aa0deae811221d7ff2cd6334e40
Suzanne Samoilova <suzelle@yandex.ru>
2021-06-17 15:54:30 +0300

    feat saveNameByClickOnSaveButton

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,11 @@
 console.log('Симпл Димпл! Нет, Поп ит!');
 
 // Выборка
+// Кнопка "Редактировать профиль"
 const popupOpenButtonElement = document.querySelector('.profile__button-edit');
+// Весь попап
 const popupElement = document.querySelector('.popup');
+// Кнопка "Закрыть попап"
 const popupCloseButtonElement = popupElement.querySelector('.popup__button-close');
 
 // Переключение всплывающего окна
@@ -24,7 +27,34 @@ const closePopupByClickOnOverlay = function (event) {
     closePopup()
 }
 
-// Открыть по клику
+// Открыть/закрыть по клику
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 popupElement.addEventListener('click', closePopupByClickOnOverlay);
+
+
+// Найти имя пользователя "Жак-Ив Кусто" и инфо пользователя "Исследователь океана"
+const profileElement = document.querySelector('.profile');
+const profileNameElement = profileElement.querySelector('.profile__name');
+const profileInfoElement = profileElement.querySelector('.profile__text');
+// console.log(profileNameElement, profileInfoElement);
+
+// Найти поле ввода имени и поле ввода информации
+const inputProfileName = popupElement.querySelector('.popup__name');
+const inputProfileInfo = popupElement.querySelector('.popup__info');
+// console.log(inputProfileName, inputProfileInfo);
+
+// Найти кнопку "Сохранить"
+const popupSaveButton = popupElement.querySelector('.popup__button-save');
+
+// Сохранить после нажатия кнопки "Сохранить"
+const saveNameByClickOnSaveButton = function () {
+    profileNameElement.textContent = inputProfileName.value;
+    profileInfoElement.textContent = inputProfileInfo.value;
+}
+
+// Закрыть и сохранить после нажатия кнопки "Сохранить"
+popupSaveButton.addEventListener('click', closePopup);
+popupSaveButton.addEventListener('click', saveNameByClickOnSaveButton);
+
+
