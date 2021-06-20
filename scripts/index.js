b9eede85cbcf3e025e59b35383019978b1677315
Suzanne Samoilova <suzelle@yandex.ru>
2021-06-20 10:23:06 +0300

    fix rewue1 in new branch

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,9 @@
-// Проверка подключения
-console.log('Симпл Димпл! Нет, Поп ит!');
-
-
 // Кнопка "Редактировать профиль"
 const popupOpenButtonElement = document.querySelector('.profile__button-edit');
 // Весь попап
 const popupElement = document.querySelector('.popup');
 // Кнопка "Закрыть попап"
 const popupCloseButtonElement = popupElement.querySelector('.popup__button-close');
-
-
-// Переключение всплывающего окна
-const openPopup = function () {
-    popupElement.classList.add('popup_opened')
-}
-
-const closePopup = function () {
-    popupElement.classList.remove('popup_opened')
-}
-
-
-//Закрыть на затемненную область
-const closePopupByClickOnOverlay = function (event) {
-    console.log(event.target, event.currentTarget)
-    if (event.target !== event.currentTarget) {
-        return
-    }
-    closePopup()
-}
-
-
-// Открыть попап по кнопке редактирования/ закрыть по кнопке крестика или затемненной области
-popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
-popupElement.addEventListener('click', closePopupByClickOnOverlay);
-
 
 // Найти имя пользователя "Жак-Ив Кусто" и инфо пользователя "Исследователь океана"
 const profileElement = document.querySelector('.profile');
@@ -43,17 +12,29 @@ const profileInfoElement = profileElement.querySelector('.profile__text');
 
 
 // Найти поле ввода имени и поле ввода информации
-const inputProfileName = popupElement.querySelector('.popup__name');
-const inputProfileInfo = popupElement.querySelector('.popup__info');
-
-
-//Значение полей ввода взято со страницы
-inputProfileName.value = profileNameElement.textContent;
-inputProfileInfo.value = profileInfoElement.textContent;
+const inputProfileName = popupElement.querySelector('.popup__text_name');
+const inputProfileInfo = popupElement.querySelector('.popup__text_info');
 
 
 // Находим форму отправки (полей ввода имени и информации) в DOM
-const formElement = document.querySelector('.popup__input');
+const formElement = document.querySelector('.popup__form');
+
+// Переключение всплывающего окна
+const openPopup = function () {
+    popupElement.classList.add('popup_opened')
+    //Значение полей ввода взято со страницы
+    inputProfileName.value = profileNameElement.textContent;
+    inputProfileInfo.value = profileInfoElement.textContent;
+}
+
+const closePopup = function () {
+    popupElement.classList.remove('popup_opened')
+}
+
+
+// Открыть попап по кнопке редактирования/ закрыть по кнопке крестика
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
 
 
 // Обработчик «отправки» формы (пока никуда отправляться не будет)
@@ -61,10 +42,10 @@ const formElement = document.querySelector('.popup__input');
         evt.preventDefault();
         profileNameElement.textContent = inputProfileName.value;
         profileInfoElement.textContent = inputProfileInfo.value;
+        closePopup();
     }
 
 
 // Обработчик формы, следит за событием “submit” - кнопка "Сохранить"
 formElement.addEventListener('submit', formSubmitHandler);
-formElement.addEventListener('submit', closePopup);
 
