1e852e48ed385350eb19351943a06b6e48d3c456
Suzanne Samoilova <suzelle@yandex.ru>
2021-07-02 16:49:22 +0300

    feat functional addNewCard

--- scripts/index.js
+++ scripts/index.js
@@ -51,7 +51,32 @@ formElement.addEventListener('submit', formSubmitHandler);
 // ___________________________________________________________________________________________________________________________
 
 
-// Добавить карточки
+
+
+
+
+
+
+
+
+
+// Лайки
+// Найти все карточки
+const cards = document.querySelectorAll('.card');
+
+const likeHandler = function (card) {
+    // Найти кнопку лайка во всех карточках
+    const buttonLike = card.querySelector('.card__button-like');
+    // Добавить класс с тёмным лайком или удалить его
+    buttonLike.addEventListener('click',() => {buttonLike.classList.toggle('card__button-like_active')});
+}
+
+// Перебрать все карточки на лайк
+cards.forEach(likeHandler);
+// ___________________________________________________________________________________________________________________________
+
+
+// Попап Добавить карточки
 // Кнопка Добавить место
 const popupOpenButtonAddPlace = document.querySelector('.profile__button-add');
 // Попап добавления места
@@ -60,36 +85,66 @@ const popupAddPlace = document.querySelector('.popup_add-card');
 const buttonClosePopupAddPlace = popupAddPlace.querySelector('.popup_button-close');
 
 // Открыть попап добавления места
-
-// const openPopupAddPlace = function () {
-//     popupAddPlace.classList.add('popup_opened')
-// }
-
 // Заменила на стрелочную функцию, проверить
 popupOpenButtonAddPlace.addEventListener('click', () => {popupAddPlace.classList.add('popup_opened')});
 
 // Закрыть попап добавления места
-
-// const closePopupAddPlace = function () {
-//     popupAddPlace.classList.remove('popup_opened')
-// }
-// buttonClosePopupAddPlace.addEventListener('click', closePopupAddPlace);
-
 // Заменила на стрелочную функцию, проверить
 buttonClosePopupAddPlace.addEventListener('click', () => {popupAddPlace.classList.remove('popup_opened')});
+
+
+//
+
+
+
+// // Найти поле имени на карточке и img для ссылки
+// const cardNamePlace = cards.querySelector('.card__text');
+// const  cardImgPlace = cards.querySelector('.card__photo');
+
+// Находим форму отправки (полей названия и ссылки) в DOM
+const formNewPlace = document.querySelector('.popup_form-add');
+
+
+// Обработчик «отправки» формы названия места и ссылки
+function formSubmitPlaceHandler (evt) {
+    evt.preventDefault();
+
+    // // Найти поля ввода названия и ссылки в попапе
+    const inputNameNewPlace = popupAddPlace.querySelector('.popup_input_name-place');
+    const inputLinkNewPlace = popupAddPlace.querySelector('.popup_input_link');
+    console.log();
+
+
+    // Добавление карточки по шаблону template
+// Нашли шаблон и обратились к его содержимому
+    const userTemplate = document.querySelector('#newcard').content;
+// Найти блок со всеми карточками, куда надо добавить новую
+    const blockAllCards = document.querySelector('.cards');
+//клонируем содержимое тега template
+    const userElement = userTemplate.querySelector('.card').cloneNode(true);
+
+
+    // наполняем содержимым
+    // userElement.querySelector('.card__photo').src = evt.AddLinkPlace;
+    // userElement.querySelector('.card__text').textContent = evt.AddNamePlace;
+
+    likeHandler(userElement);
+
+// отображаем на странице
+    blockAllCards.prepend(userElement);
+
+    closePopup();
+}
+
+// Обработчик формы, следит за событием “submit” - кнопка "Создать"
+formNewPlace.addEventListener('submit', formSubmitPlaceHandler);
 // ___________________________________________________________________________________________________________________________
 
 
-// Найти все карточки
-const cards = document.querySelectorAll('.card');
+// ДОДЕЛАТЬ
 
-// Перебрать все карточки на лайк
-cards.forEach(function (card) {
-    // Найти кнопку лайка во всех карточках
-    const buttonLike = card.querySelector('.card__button-like');
-    // Добавить класс с тёмным лайком или удалить его
-    buttonLike.addEventListener('click',() => {buttonLike.classList.toggle('card__button-like_active')});
-});
+
+
 // ___________________________________________________________________________________________________________________________
 
 
